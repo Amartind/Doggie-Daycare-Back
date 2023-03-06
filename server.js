fa4b0bced7d41b3e010261f05c5d4cceeb528288
Amartind <112136243+Amartind@users.noreply.github.com>
2023-03-06 12:23:31 -0800

    add Pets model

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const allRoutes = require('./controllers');
+const sequelize = require('./config/connection');
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Requiring our models for syncing
+const { variable} = require('./models');
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/',allRoutes);
+
+app.get('/',(req,res)=>{
+res.send('hello welcome')
+})
+
+sequelize.sync({ force: true }).then(function() {
+app.listen(PORT, function() {
+console.log('App listening on PORT ' + PORT);
+});
+});
