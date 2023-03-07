065e4c2322d9d37a83b820bb8008c17672363a5d
Emoses95 <moses.e.collins@gmail.com>
2023-03-07 13:48:48 -0800

    deleted routes folder moved files into controller folder

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,54 +1,61 @@
 const router = require('express').Router();
-const Pet = require('../models/Pet');
+const { Pet, User } = require('../models');
 
+router.post('/', (req, res) => {
+    Pet.create(req.body)
+        .then((createPet) => {
+            res.status(200).json(createPet)
+        })
+})
+
+// Find all pets 
 router.get('/', (req, res) => {
-  Pet.findAll().then((petData) => {
-    res.json(petData);
-  });
-});
 
-router.get('/:id', (req, res) => {
-  Pet.findOne(
-    {
-      where: { 
-        id: req.params.id 
-      },
-    }
-  ).then((petData) => {
-    res.json(petData);
-  });
-});
-
-router.put('/:id', (req, res) => {
-  Pet.update(
-    {
-      name: req.body.name,
-      age: req.body.age,
-      breed: req.body.breed,
-      personality: req.body.personality,
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
-    .then((updatedPet) => {
-      res.json(updatedPet);
+    Pet.findAll({
+        include: [User]
+    }).then((petData) => {
+        res.json(petData)
     })
-    .catch((err) => res.json(err));
 });
 
+// Find pet by id
+router.get('/:id', (req, res) => {
+
+    Pet.findByPk(req.params.id, {
+        include: [User]
+    }).then((petData) => {
+        res.json(petData)
+    })
+});
+
+//Update pet 
+router.put('/:id', (req, res) => {
+
+    Pet.update({
+        name: req.body.name,
+        age: req.body.age,
+        breed: req.body.breed,
+        personality: req.body.personality
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then((updatedPet) => {
+        res.json(updatedPet)
+    })
+        .catch((err) => res.json(err));
+});
+
+// Delete pet
 router.delete('/:id', (req, res) => {
     Pet.destroy({
-      where: {
-        id: req.params.id,
-      },
+        where: {
+            id: req.params.id
+        }
+    }).then((deletedPet) => {
+        res.json(deletedPet)
     })
-      .then((deletedPet) => {
-        res.json(deletedPet);
-      })
-      .catch((err) => res.json(err));
-  });
+        .catch((err) => res.json(err));
+});
 
 module.exports = router;
